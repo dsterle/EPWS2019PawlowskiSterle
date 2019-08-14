732b1da0fa22b9242de29df321db6740d8d92a82
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-08-14 19:12:55 +0200

    project setup

---
+++ frontend-website/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Project Setup</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {}
+};
+</script>
+
+<style lang="scss">
+@import "./scss/site.scss";
+</style>
