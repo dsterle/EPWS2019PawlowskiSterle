50f34c9e2a4d7278a6074d9c6875bd91bab66240
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-08-15 20:51:01 +0200

    added routing

--- frontend-website/src/App.vue
+++ frontend-website/src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div id="app">
-    <h1>Project Setup</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-export default {
-  name: "app",
-  components: {}
-};
-</script>
 
 <style lang="scss">
 @import "./scss/site.scss";
