f396cdf76536db18ed43ee1a140475272a67ab1e
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-08-16 11:06:23 +0200

    worked on different text forms

--- frontend-website/src/router.js
+++ frontend-website/src/router.js
@@ -14,13 +14,23 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/waiting',
+      name: 'waiting',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: function () {
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
+        return import(/* webpackChunkName: "about" */ './views/Waiting.vue')
+      }
+    },
+    {
+      path: '/painting',
+      name: 'painting',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: function () {
+        return import(/* webpackChunkName: "about" */ './views/Painting.vue')
       }
     }
   ]
