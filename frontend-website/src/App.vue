1b24fd78425ea0fa1ab502daa2bd1020e4cf7755
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-08-16 10:51:36 +0200

    added variables and fonts

--- frontend-website/src/App.vue
+++ frontend-website/src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- <div id="nav"> -->
+    <!-- <router-link to="/">Home</router-link>| -->
+    <!-- <router-link to="/about">About</router-link> -->
+    <h1 class="headline">Headline</h1>
+    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, rem.</p>
+    <p class="button-text">Button Text</p>
+    <p class="hint-text">Hint Text</p>
+    <p class="error-text">Error Text</p>
+    <p class="description-text">Description Text</p>
+    <!-- </div> -->
+    <router-view />
   </div>
 </template>
 
--- frontend-website/src/views/Home.vue
+++ frontend-website/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1></h1>
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
