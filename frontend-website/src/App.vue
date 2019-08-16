84f2e1f6acdfa7675366d914544daff5f7d36f02
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-08-16 13:16:12 +0200

    changed typo and created Home page

--- frontend-website/src/App.vue
+++ frontend-website/src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
+    <!-- <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
+    />-->
     <router-view />
   </div>
 </template>
 
+<script>
+export default {};
+</script>
+
 <style lang="scss">
-@import "./scss/site.scss";
+@import url("https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css");
+@import "./src/assets/scss/site.scss";
 </style>
--- frontend-website/src/views/Home.vue
+++ frontend-website/src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="home">
-    <h1 class="headline">Headline</h1>
+    <div class="home-wrapper">
+      <h1 class="headline welcome-text">
+        Willkommen im
+        <br />Lukas Cranach
+        <br />Archiv
+      </h1>
+      <p class="text note-text">Bitte geben Sie die Nummer von Ihrem Transponder ein</p>
+    </div>
+    <!-- <h1 class="headline">Headline</h1>
     <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, rem.</p>
     <p class="button-text">Button Text</p>
     <p class="hint-text">Hint Text</p>
     <p class="error-text">Error Text</p>
-    <p class="description-text">Description Text</p>
+    <p class="description-text">Description Text</p>-->
   </div>
 </template>
 
@@ -15,3 +23,20 @@ export default {
   components: {}
 };
 </script>
+
+<style lang="scss">
+@import "../assets/scss/010-variables.scss";
+
+.home-wrapper {
+  padding-left: $abstand-L;
+  padding-right: $abstand-L;
+
+  .welcome-text {
+    margin-top: $abstand-XXL;
+  }
+
+  .note-text {
+    margin-top: $abstand-XL;
+  }
+}
+</style>
