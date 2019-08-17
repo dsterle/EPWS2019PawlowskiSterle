8cddb58b1ae465b63546c43e85639f8d329324ce
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-08-17 12:09:36 +0200

    created Waiting page

--- frontend-website/src/views/Waiting.vue
+++ frontend-website/src/views/Waiting.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="waiting">
-    <h1 class="headline">Waiting</h1>
+    <div class="app-bar-wrapper">
+      <a class="back-icon" href="/">
+        <img src="../assets/icons/arrow_back.svg" />
+      </a>
+    </div>
+    <div class="content-wrapper">
+      <p class="hint-text note-text">Halten Sie Ihren Transponder vor ein Gemälde...</p>
+    </div>
   </div>
 </template>
 
@@ -10,3 +17,32 @@ export default {
   components: {}
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/010-variables.scss";
+
+.app-bar-wrapper {
+  display: flex;
+  height: $app-bar-height;
+  width: 100%;
+  align-items: center;
+
+  .back-icon {
+    margin-left: $abstand-M;
+  }
+}
+
+.content-wrapper {
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .note-text {
+    width: 80%;
+    position: relative;
+    text-align: center;
+  }
+}
+</style>
