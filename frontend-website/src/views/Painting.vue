aa22c9b966753186bd9fdb932b5c78c8126f7e91
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-08-19 15:36:12 +0200

    created Painting page

--- frontend-website/src/views/Painting.vue
+++ frontend-website/src/views/Painting.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="painting">
-    <h1 class="headline">Painting</h1>
+    <img
+      src="http://lucascranach.org/thumbnails/DE_BStGS_1130_FR285E/01_Overall/DE_BStGS_1130_FR285E_2010_Overall.jpg"
+      alt
+    />
+    <div class="content-wrapper">
+      <h1 class="painting-title">The Crucifixion of Christ, the so-called Schottenkreuzigung</h1>
+      <p class="hint-text year">1552</p>
+    </div>
   </div>
 </template>
 
@@ -10,3 +17,20 @@ export default {
   components: {}
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/010-variables.scss";
+
+img {
+  width: 100%;
+  height: 100%;
+}
+
+.content-wrapper {
+  padding: $abstand-M $abstand-M 0 $abstand-M;
+
+  .year {
+    margin-top: $abstand-S;
+  }
+}
+</style>
