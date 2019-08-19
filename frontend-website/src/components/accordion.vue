2af20dd63d2ea58a5d4eead984c4de0e7cadc0b4
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-08-19 20:02:21 +0200

    accordion can open and close (with transition effects)

--- frontend-website/src/components/accordion.vue
+++ frontend-website/src/components/accordion.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="accordion">
-    <button class="accordion-button" v-on:click="open">Kurzbeschreibung</button>
+    <div class="accordion-head">
+      <div class="icon-wrapper">
+        <img
+          class="icon"
+          src="../assets/icons/arrow_down.svg"
+          alt="arrow down"
+          v-on:click="open"
+          js-accordion="icon"
+        />
+      </div>
+      <input
+        type="button"
+        class="text accordion-title"
+        value="Kurzbeschreibung"
+        js-accordion="title"
+        v-on:click="playAudio"
+      />
+    </div>
     <p class="description-text accordion-text" js-accordion="text">{{ item.text }}</p>
   </div>
 </template>
@@ -12,14 +29,22 @@ export default {
     open() {
       // $el gives the DOM elements of this
       var text = this.$el.querySelector("[js-accordion=text]");
+      var icon = this.$el.querySelector("[js-accordion=icon]");
 
       if (text.style.maxHeight) {
         // accordion is open
+        icon.classList.remove("turn-icon");
+        text.classList.remove("visible");
         text.style.maxHeight = null;
       } else {
         // accordion is closed
+        icon.classList.add("turn-icon");
+        text.classList.add("visible");
         text.style.maxHeight = text.scrollHeight + "px";
       }
+    },
+    playAudio() {
+      // TODO implement
     }
   }
 };
@@ -29,15 +54,41 @@ export default {
 @import "../assets/scss/010-variables.scss";
 
 .accordion {
-  .accordion-button {
+  .accordion-head {
+    display: flex;
     width: 100%;
-    text-align: left;
-    height: $accordion-height;
+    height: 1rem * 3;
+
+    .icon-wrapper {
+      display: flex;
+      width: $icon-wrapper-width;
+    }
+
+    .accordion-title {
+      padding: 0;
+    }
   }
   .accordion-text {
     max-height: 0;
     overflow: hidden;
     transition: 0.3s;
   }
+}
+
+.visible {
+  margin: $abstand-S $abstand-S $abstand-L $icon-wrapper-width;
+}
+
+.icon {
+  transition: 0.3s;
+}
+
+.turn-icon {
+  // transition: 0.3s;
+  transform: rotate(180deg);
+}
+
+.currentAudio {
+  color: $accent;
 }
 </style>
