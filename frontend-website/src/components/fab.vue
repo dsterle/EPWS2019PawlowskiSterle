2940a75cada4364a6943197fab2e2de6a53cac09
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-08-30 09:58:45 +0200

    outsourced methd playAudio in Painting.vue

--- frontend-website/src/components/fab.vue
+++ frontend-website/src/components/fab.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="floating-action-button">
-    <img class="icon" v.bind:src="this.src" alt="Pause Button" />
+    <img js-icon v-bind:src="src" v-bind:alt="alt" v-on:click="$emit('fab-clicked')" />
   </div>
 </template>
     
 <script>
 export default {
   name: "fab",
-  props: { src }
+  props: ["src", "alt"]
 };
 </script>
 
@@ -22,10 +22,8 @@ export default {
   align-items: center;
   justify-content: center;
   border-radius: 100%;
-  position: fixed;
   right: 0;
   bottom: 0;
-  margin: 0 $abstand-M $abstand-M 0;
   box-shadow: 0px 14px 14px rgba(0, 0, 0, 0.237602),
     0px 0px 14px rgba(0, 0, 0, 0.12);
 }
