5ac4569d95e56a53cc5b5d6ec28e7464e6daed9b
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-09-01 20:47:12 +0200

    created audioSlider for underneath the accordion title

---
+++ frontend-website/src/components/audioSlider.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="audio-slider">
+    <div v-if="current" class="line"></div>
+    <div v-if="current" class="current-dot"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "audioSlider",
+  props: ["current"]
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/010-variables.scss";
+
+.audio-slider {
+  display: flex;
+  align-items: center;
+
+  .line {
+    width: 50%;
+    height: 1px;
+    background: $accent;
+  }
+
+  .current-dot {
+    width: 10px;
+    height: 10px;
+    border-radius: 10px;
+    background: $accent;
+  }
+}
+</style>
