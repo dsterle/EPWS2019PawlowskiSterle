032f7d8172761f63ac108809e19f85b96ad0c064
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-12-02 14:53:28 +0100

    added margin to inputField

--- frontend-website/src/views/Home.vue
+++ frontend-website/src/views/Home.vue
@@ -3,8 +3,9 @@
     <div class="welcome-text">
       <h1 class="headline">Willkommen im Lucas Cranach Archiv</h1>
     </div>
-    <p class="normal-text note-text">Bitte geben Sie die Nummer von Ihrem Transponder ein</p>
+    <!-- <p class="normal-text note-text">Bitte geben Sie die Nummer von Ihrem Transponder ein</p> -->
     <inputField
+      class="inputField"
       inputID="input-transponder"
       inputPlaceholder="Chip Nummer"
       buttonText="Ok"
@@ -53,6 +54,10 @@ export default {
     margin-top: $abstand-XXL;
   }
 
+  .inputField {
+    margin-top: $abstand-XL;
+  }
+
   .note-text {
     margin-top: $abstand-XL;
     margin-bottom: 0;
@@ -60,16 +65,6 @@ export default {
 
   ol {
     list-style-type: "decimal";
-  }
-
-  .docs-button {
-    color: $accent;
-    font-size: $font-size-S;
-    background: none;
-    border: none;
-    position: absolute;
-    bottom: 45px;
-    padding: 0;
   }
 }
 </style>
