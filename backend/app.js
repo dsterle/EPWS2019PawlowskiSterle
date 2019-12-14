e63549e6906661192b92e1b251cfa72e2e55e8cd
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-12-14 17:37:50 +0100

    init

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var graphqlHTTP = require("express-graphql");
+var { buildSchema } = require("graphql");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.listen(4000);
+
+console.log("Server is running\nPort 4000");
