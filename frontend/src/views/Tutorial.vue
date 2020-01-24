38424a692a2414738cea685e8afab81232a7eda6
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-01-24 21:10:59 +0100

    added slider

--- frontend/src/views/Tutorial.vue
+++ frontend/src/views/Tutorial.vue
@@ -2,18 +2,17 @@
   <div class="container">
     <div js-page class="page one active">
       <p class="normal-text">Page One</p>
-      <button v-on:click="plusSlide(-1)">Prev</button>
-      <button v-on:click="plusSlide(1)">Next</button>
+      <button v-on:click="showSlide(1)">Next</button>
     </div>
     <div js-page class="page two">
       <p class="normal-text">Page Two</p>
-      <button v-on:click="plusSlide(-1)">Prev</button>
-      <button v-on:click="plusSlide(1)">Next</button>
+      <button v-on:click="showSlide(-1)">Prev</button>
+      <button v-on:click="showSlide(1)">Next</button>
     </div>
     <div js-page class="page three">
       <p class="normal-text">Page Three</p>
-      <button v-on:click="plusSlide(-1)">Nein</button>
-      <button v-on:click="plusSlide(1)">Ja</button>
+      <button v-on:click="showSlide(-1)">Nein</button>
+      <button v-on:click="showSlide(1)">Ja</button>
     </div>
   </div>
 </template>
@@ -22,6 +21,7 @@
 export default {
   data() {
     return {
+      currentSlide: 0,
       slides: []
     };
   },
@@ -30,7 +30,12 @@ export default {
     console.log(this.slides);
   },
   methods: {
-    showSlide(n) {}
+    showSlide(n) {
+      this.slides[this.currentSlide].classList.remove("active");
+      this.currentSlide += n;
+      console.log("currentSlide " + this.currentSlide);
+      this.slides[this.currentSlide].classList.add("active");
+    }
   }
 };
 </script>
