a1cb554b0a8844389c6c33813c21c8fde9a8ad17
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-01-25 17:11:36 +0100

    added tutorial screens as components

---
+++ frontend/src/components/tutorial-screens/first.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p class="normal-text">Guten Tag!</p>
+    <p class="normal-text">Ich bin Ihr Museums Assistent</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ frontend/src/components/tutorial-screens/second.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p
+      class="normal-text"
+    >Sollten Sie Informationen zu einem Gemälde lesen oder hören wollen, müssen Sie nur Ihren Chip an das jeweilige Lesegerät neben dem Gemälde halten</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
