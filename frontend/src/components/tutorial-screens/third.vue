a421be936e019640211ad610bb985a49789e2acd
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-01-25 19:38:18 +0100

    created third tutorial screen

--- frontend/src/components/tutorial-screens/third.vue
+++ frontend/src/components/tutorial-screens/third.vue
@@ -1,8 +1,50 @@
-<template></template>
+<template>
+  <div class="container">
+    <p
+      class="normal-text"
+    >Wenn Sie möchten kann ich Ihnen die Informationen auch vorlesen, dann können Sie Kopfhörer anschließen und Ihr Handy in der Hosentasche lassen</p>
+    <div class="button-wrapper">
+      <div class="button --grey">
+        <p class="button-text">Nein</p>
+      </div>
+      <div class="button">
+        <p class="button-text">Ja</p>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {};
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/scss/010-variables.scss";
+
+.container {
+  .button-wrapper {
+    width: 100%;
+    justify-content: space-between;
+    margin-top: $abstand-L;
+    display: flex;
+
+    .button {
+      display: flex;
+      justify-content: center;
+      width: $button-width;
+      background: $accent;
+      padding: $abstand-M 0;
+      border-radius: 5px;
+      color: $lighter;
+      cursor: pointer;
+      transition: 0.5s;
+      box-shadow: 0 14px 14px rgba(0, 0, 0, 0.237602),
+        0 0 14px rgba(0, 0, 0, 0.12);
+    }
+
+    .button.--grey {
+      background: $light;
+    }
+  }
+}
 </style>
