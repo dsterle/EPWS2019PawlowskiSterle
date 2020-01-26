db398494eaeaaf5e24676ee93eb464c294ea3db2
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-01-26 22:07:58 +0100

    changed text

--- frontend/src/components/tutorial-screens/second.vue
+++ frontend/src/components/tutorial-screens/second.vue
@@ -5,7 +5,6 @@
     >Wenn Sie Informationen zu einem Gemälde lesen oder hören wollen, müssen Sie nur Ihren Chip an das dazugehörige Lesegerät halten</p>
     <div class="illustration-wrapper">
       <img src="../../assets/illustrations/handwithchip.svg" alt />
-      <!-- <img src="../../assets/illustrations/painting.svg" alt /> -->
     </div>
   </div>
 </template>
