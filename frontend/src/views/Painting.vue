9be9b82df843fc4a237f547bf9e1738bb0d2cec1
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-02-21 12:19:32 +0100

    ip adresse in request geändert

--- frontend/src/views/Painting.vue
+++ frontend/src/views/Painting.vue
@@ -82,7 +82,7 @@ export default {
     let _this = this;
     let result = await axios({
       method: "POST",
-      url: "http://192.168.178.70:4000/graphql",
+      url: "http://localhost/graphql",
       data: {
         query: `
             {
