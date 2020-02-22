04aa8bf1929aeae9ff51e2a9de263dc8fbfea860
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-02-23 00:12:02 +0100

    new style

--- frontend/src/components/settingButton.vue
+++ frontend/src/components/settingButton.vue
@@ -1,41 +1,41 @@
 <template>
   <div class="setting" @click="$router.push({name: page})">
-      <span class="setting-description">
-        <i v-bind:class="icon"></i>
-        {{description}}
-        <i class="fas fa-chevron-right"></i>
-      </span>
+    <div class="left">
+      <i v-bind:class="icon"></i>
+      <p class="text button-text setting-description">{{description}}</p>
+    </div>
+    <i class="fas fa-chevron-right"></i>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "settingButton",
-        props: ["description", "page", "icon"],
-        mounted() {
-
-        }, created() {
-
-        }
-    }
+export default {
+  name: "settingButton",
+  props: ["description", "page", "icon"],
+  mounted() {},
+  created() {}
+};
 </script>
 
 <style lang="scss" scoped>
-  @import "../assets/scss/010-variables.scss";
-  .setting {
-    padding: $abstand-M;
-    width: 100%;
-    font-size: $font-size-L;
-    color: $lighter;
-    border-bottom: 1px solid $light;
-    span {
-      i {
-        margin-right: $abstand-M;
-      }
-      .fa-chevron-right {
-        position: absolute;
-        right: $abstand-S;
-      }
+@import "../assets/scss/010-variables.scss";
+
+.setting {
+  padding: $card-padding;
+  background: $dark;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 5px;
+  box-shadow: 0px 5px 8px rgba(0, 0, 0, 0.5);
+
+  .left {
+    align-items: center;
+    display: flex;
+
+    .text {
+      margin-left: $abstand-M;
     }
   }
+}
 </style>
