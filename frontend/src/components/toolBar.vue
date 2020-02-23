5267a63fb8bf1d7cea0b72ebf1e6232e9116f440
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-02-23 17:51:16 +0100

    removed unnecessary code

--- frontend/src/components/toolBar.vue
+++ frontend/src/components/toolBar.vue
@@ -1,20 +1,5 @@
 <template>
   <div class="toolBar">
-    <!-- <i class="fas fa-home" @click="$router.push({name: 'waiting', params: {userid: user}})"></i> -->
-    <!-- <i class="fas fa-search" @click="$router.push({name: 'search'})"></i> -->
-    <!-- <i
-      v-if="!isNaN(currentPageAvailable)"
-      class="fas fa-image"
-      @click="$router.push({name: 'painting', params: {userid: user, id: paintingId}})"
-    ></i>-->
-    <!-- <i
-      v-else
-      class="fas fa-image"
-      @click="$router.push({name: 'noPainting', params: {userid: user}})"
-    ></i>-->
-    <!-- <i class="fas fa-history" @click="$router.push({name: 'history', params: {userid: user}})"></i> -->
-    <!-- <i class="fas fa-cog" @click="$router.push({name: 'settings', params: {userid: user}})"></i> -->
-
     <img
       js-home
       class="active"
