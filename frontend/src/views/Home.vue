4a667621e194d06716e8f9420e0398843f11f813
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-02-23 17:16:30 +0100

    tested fullscreen but did not wrk

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -14,8 +14,7 @@
       v-bind:error="errorMsg"
       v-on:event-clicked="validateIDs"
     ></inputField>
-
-    <!-- <input type="button" value="Tutorial starten" class="button-text tutorialButton" @click="$router.push({name: 'tutorial'})"> -->
+    <button type="button" @click="toggle">Fullscreen</button>
   </div>
 </template>
 
