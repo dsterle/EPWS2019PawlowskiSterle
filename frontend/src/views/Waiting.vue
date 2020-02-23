e46664f5d4b74c8cf7528188d173208e01dc9be6
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-02-23 19:31:48 +0100

    removed comment

--- frontend/src/views/Waiting.vue
+++ frontend/src/views/Waiting.vue
@@ -14,7 +14,6 @@
 </template>
 
 <script>
-// import handleMQTTConnection from "../assets/js/handleMQTTConnection";
 import toolBar from "../components/toolBar.vue";
 import headBar from "../components/headBar";
 
