ed85413053a1d4d6ebad37869fe9fa5817ea7db1
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-02-23 01:34:40 +0100

    changed style

--- frontend/src/components/settingButton.vue
+++ frontend/src/components/settingButton.vue
@@ -27,8 +27,8 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border-radius: 5px;
-  box-shadow: 0px 5px 8px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  box-shadow: 0px 5px 8px rgba(0, 0, 0, 0.25);
 
   .left {
     align-items: center;
