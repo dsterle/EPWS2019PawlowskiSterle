6828266a71a3a5bf6b74862ecc1a09af771574e3
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2020-02-23 01:33:54 +0100

    changed shadows

--- frontend/src/components/toolBar.vue
+++ frontend/src/components/toolBar.vue
@@ -99,7 +99,8 @@ export default {
   background: $darker;
   color: $lighter;
   z-index: 5;
-  box-shadow: 0px -5px 8px rgba(0, 0, 0, 0.5);
+  // border-top: 2px solid $dark;
+  box-shadow: 0px -5px 8px rgba(0, 0, 0, 0.25);
 
   .inActive {
     opacity: 0.3;
